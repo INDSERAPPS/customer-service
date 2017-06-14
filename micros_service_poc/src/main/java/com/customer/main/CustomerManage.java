afe7c278d3e0beca6cc6ad2f7cc40fa20896ba0e
abinstephen1989 <astephen@interac.ca>
2017-06-14 13:10:32 +0530

    Update CustomerManage.java
    Added a space to check Jenkins

--- micros_service_poc/src/main/java/com/customer/main/CustomerManage.java
+++ micros_service_poc/src/main/java/com/customer/main/CustomerManage.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
